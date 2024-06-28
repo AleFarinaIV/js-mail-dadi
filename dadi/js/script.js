d6785166921c4a34224bf369b1b7050b86f87fb2
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-06-28 18:19:13 +0200

    [UPD] linked bootstrap, created index structure and created IF statement

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,4 +3,8 @@ const button = document.getElementById('button');
 button.addEventListener('click', function () {
     let user = Math.floor(Math.random() * 6 + 1)
     let computer = Math.floor(Math.random() * 6 + 1)
+    console.log(user, computer)
+    if (user > computer) {
+        document.getElementById('userWin').innerHTML = "Complimenti hai vinto"
+    }
 })
