2735251944ac7b3b5472f647213f0c105bca3709
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-06-28 17:58:16 +0200

    [UPD] added EventListener method and variables for user and CPU

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,6 @@
 const button = document.getElementById('button');
+
+button.addEventListener('click', function () {
+    let user = Math.floor(Math.random() * 6 + 1)
+    let computer = Math.floor(Math.random() * 6 + 1)
+})
