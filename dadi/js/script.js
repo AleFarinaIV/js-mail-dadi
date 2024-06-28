a7e6fb660a9b320a5258a7b5e555d939932bd3ea
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-06-28 18:43:13 +0200

    [UPD] corrected id names and finished exercise

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -5,6 +5,16 @@ button.addEventListener('click', function () {
     let computer = Math.floor(Math.random() * 6 + 1)
     console.log(user, computer)
     if (user > computer) {
-        document.getElementById('userWin').innerHTML = "Complimenti hai vinto"
+        document.getElementById('userMessage').innerHTML = "Complimenti hai vinto!!"
+        document.getElementById('cpuMessage').innerHTML = "Error 404 NoT FounD"
+        document.getElementById('draw').innerHTML = ""
+    }else if (user < computer) {
+        document.getElementById('userMessage').innerHTML = "Mi dispiace, ha vinto il computer :("
+        document.getElementById('cpuMessage').innerHTML = "Hai vinto contro l'utente!!"
+        document.getElementById('draw').innerHTML = ""
+    }else {
+        document.getElementById('userMessage').innerHTML = ""
+        document.getElementById('cpuMessage').innerHTML = ""
+        document.getElementById('draw').innerHTML = "SIETE RIUSCITI A PAREGGIARE!!"
     }
 })
